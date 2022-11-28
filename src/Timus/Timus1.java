e4be9a3577826dc230e1fe5f2378fdc592d179ce
Kostya <skavarodka74@gmail.com>
2022-11-28 16:42:22 +0500

    timus_ready

--- src/Timus/Timus1.java
+++ src/Timus/Timus1.java
@@ -1,7 +1,16 @@
 package Timus;
-
+import java.io.PrintWriter;
+import java.util.Scanner;
 public class Timus1 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int res = N * A * B * 2;
+        out.println(res);
+        out.flush();
 
     }
 }
