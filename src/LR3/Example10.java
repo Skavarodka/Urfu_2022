8f21946993af5fc446b1c8683dc0bfcccd02cb67
Kostya <skavarodka74@gmail.com>
2022-12-10 16:52:47 +0500

    LR3 done

--- src/LR3/Example10.java
+++ src/LR3/Example10.java
@@ -1,6 +1,5 @@
 package LR3;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 public class Example10 {
@@ -9,14 +8,20 @@ public class Example10 {
         System.out.println("Введите размер массива");
         int size = in.nextInt();
         System.out.println("Размер введенного массива равен " + size);
-        Integer[] num = new Integer[size];
+        int[] num = new int [size];
         Random random = new Random();
         for (int i = 0 ; i < num.length ; i++) {
             num[i] = random.nextInt(200);
             System.out.println("Элемент массива ["+i+"] = " + num[i]);
         }
-
-        Arrays.sort(num, Collections.reverseOrder());
+        Arrays.sort(num);
+        int n = num.length;
+        int temp;
+        for (int i = 0; i < n/2; i++) {
+            temp = num[n-i-1];
+            num[n-i-1] = num[i];
+            num[i] = temp;
+        }
         System.out.println("Произведена сортировка массива");
         for (int i = 0 ; i < num.length ; i++) {
             System.out.println("Элемент массива ["+i+"] после сортировки = " + num[i]);
